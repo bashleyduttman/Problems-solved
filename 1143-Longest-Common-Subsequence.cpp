1d1cc650718f87823240764d707a0be7e9bcc789
Deepak Saarthi P P <165629999+bashleyduttman@users.noreply.github.com>
2024-08-12 22:17:07 +0530

    [1143] [Time Beats: 26.15%] [Memory Beats: 13.74%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         
-        int n = text1.size();
-        int n2 = text2.size();
+         int n = text1.size();
+        int m = text2.size();
         
         // DP table where dp[i][j] represents the length of LCS of text1[0..i-1] and text2[0..j-1]
-        vector<vector<int>> dp(n + 1, vector<int>(n2 + 1, 0));
+        vector<vector<int>> dp(n, vector<int>(m, -1));
         
-        // Build the DP table iteratively
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+        return lcs(n - 1, m - 1, text1, text2, dp);
+    }
+
+private:
+    int lcs(int ind1, int ind2, const string& s1, const string& s2, vector<vector<int>>& dp) {
+        // Base case: when either index is out of bounds, the LCS length is 0
+        if (ind1 < 0 || ind2 < 0) {
+            return 0;
         }
-        
-        // The length of the longest common subsequence is in dp[n][n2]
-        return dp[n][n2];
+
+        // If already computed, return the stored result
+        if (dp[ind1][ind2] != -1) {
+            return dp[ind1][ind2];
+        }
+
+        // If characters match, move diagonally left-up in the grid
+        if (s1[ind1] == s2[ind2]) {
+            return dp[ind1][ind2] = 1 + lcs(ind1 - 1, ind2 - 1, s1, s2, dp);
+        }
+
+        // If characters don't match, take the maximum LCS by either:
+        // 1. Ignoring the current character of s1 and moving left
+        // 2. Ignoring the current character of s2 and moving up
+        return dp[ind1][ind2] = max(lcs(ind1 - 1, ind2, s1, s2, dp), lcs(ind1, ind2 - 1, s1, s2, dp));
     }
 };
