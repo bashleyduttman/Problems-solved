5bebf9f588bdb140e8c05bcd0f39706d396c5768
Deepak Saarthi P P <165629999+bashleyduttman@users.noreply.github.com>
2024-09-13 21:21:03 +0530

    [540] [Time Beats: 89.06%] [Memory Beats: 57.71%] - LeetPush

--- 540-Single-Element-in-a-Sorted-Array.cpp
+++ 540-Single-Element-in-a-Sorted-Array.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int l=0,r=nums.size()-1;
-        if(r==0){
-            return nums[0];
+        int l = 0, r = nums.size() - 1;
 
+        if (r == 0) {
+            return nums[0];
         }
-        while(l<=r){
-            int mid=(l+(r-l)/2);
-            cout<<nums[mid]<<'\
-';
-            if(mid==0 && nums[mid]!=nums[mid+1]){
+        if (nums[0]!=nums[1]) {
+            return nums[0];
+        }
+        if (nums[r]!=nums[r-1]) {
+            return nums[r];
+        }
+        while (l <= r) {
+            int mid = (l + (r - l) / 2);
+           
+
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
-            if(mid==nums.size()-1 && nums[mid]!=nums[mid-1]){
-                return nums[mid];
-            }
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            if((mid%2==0 && nums[mid+1]!=nums[mid]) || ((mid%2)==1 && nums[mid+1]==nums[mid])){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
+            if ((mid % 2 == 0 && nums[mid + 1] != nums[mid]) ||
+                ((mid % 2) == 1 && nums[mid + 1] == nums[mid])) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return 0;
     }
-    
 };
