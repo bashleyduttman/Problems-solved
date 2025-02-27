21a79c1721c1f65a3c67de154d09caccf299b608
Deepak Saarthi P P <165629999+bashleyduttman@users.noreply.github.com>
2025-02-27 15:12:57 +0530

    Time: 3 ms (47.09%), Space: 9.2 MB (53.4%) - LeetHub

--- 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
     int minimumBeautifulSubstrings(string s) {
-        vector<int>dp(s.size(),-1);
-        long long val=minimum(0,s,dp);
-        
-        return val>=1e7?-1:val;
-    }
-    long long minimum(int ind,string s,vector<int>&dp){
-        if(ind>=s.size()){
-            return 0;
+        int n = s.size();
+        vector<int> dp(n + 1, -1);  // dp[i] will store the minimum substrings for s[i...n-1]
+        dp[n] = 0;  // base case: no substring required for the empty string
+
+        // Precompute powers of 5 up to the maximum possible number (within range of binary string)
+        set<int> powersOfFive;
+        for (int i = 1; i <= 1e6; i *= 5) {
+            powersOfFive.insert(i);
         }
-        if(s[ind]=='0'){return 1e7;}
-        if(dp[ind]!=-1)return dp[ind];
-        string temp;
-        long long mini=1e7;
-        for(int i=ind;i<s.size();i++){
-            temp+=s[i];
-            if(check(temp)){
-               mini=min(mini,minimum(i+1,s,dp)+1);
+
+        return minimum(0, s, dp, powersOfFive);
+    }
+    
+    int minimum(int ind, const string& s, vector<int>& dp, const set<int>& powersOfFive) {
+        if (ind >= s.size()) {
+            return 0;  // No more substrings needed
+        }
+        if (dp[ind] != -1) {
+            return dp[ind];  // Return memoized value
+        }
+        
+        int mini = INT_MAX;
+        int currentValue = 0;  // To accumulate binary value
+        
+        // Try all substrings starting from index 'ind'
+        for (int i = ind; i < s.size(); i++) {
+            // If the binary number has leading zero, break early
+            if (s[ind] == '0' && i == ind) {
+                break;
+            }
+            
+            // Shift the currentValue left by 1 and add the new bit (s[i])
+            currentValue = (currentValue << 1) | (s[i] - '0');
+            
+            // If the current value is a power of 5, consider it as a valid substring
+            if (powersOfFive.find(currentValue) != powersOfFive.end()) {
+                int subResult = minimum(i + 1, s, dp, powersOfFive);
+                if (subResult != -1) {
+                    mini = min(mini, subResult + 1);  // Add one for the current valid substring
+                }
             }
         }
-        return dp[ind]=mini;
+        
+        dp[ind] = (mini == INT_MAX) ? -1 : mini;  // Memoize the result for current index
+        return dp[ind];
     }
-    bool check(string& binary_str) {
-    
-    int num = stoi(binary_str, nullptr, 2); 
-    
-
-    if (num <= 0) {
-        return false;
-    }
-    
-    while (num % 5 == 0) {
-        num /= 5;
-    }
-    
-    return num == 1;
-}
 };
