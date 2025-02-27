da0a5a76dbdc97009ce542ef3598e132f0812687
Deepak Saarthi P P <165629999+bashleyduttman@users.noreply.github.com>
2025-02-27 15:11:31 +0530

    Time: 3 ms (47.09%), Space: 10.8 MB (9.22%) - LeetHub

--- 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int minimumBeautifulSubstrings(string s) {
-        long long val=minimum(0,s);
+        vector<int>dp(s.size(),-1);
+        long long val=minimum(0,s,dp);
+        
         return val>=1e7?-1:val;
     }
-    long long minimum(int ind,string s){
+    long long minimum(int ind,string s,vector<int>&dp){
         if(ind>=s.size()){
             return 0;
         }
         if(s[ind]=='0'){return 1e7;}
+        if(dp[ind]!=-1)return dp[ind];
         string temp;
         long long mini=1e7;
         for(int i=ind;i<s.size();i++){
             temp+=s[i];
             if(check(temp)){
-               mini=min(mini,minimum(i+1,s)+1);
+               mini=min(mini,minimum(i+1,s,dp)+1);
             }
         }
-        return mini;
+        return dp[ind]=mini;
     }
     bool check(string& binary_str) {
     
