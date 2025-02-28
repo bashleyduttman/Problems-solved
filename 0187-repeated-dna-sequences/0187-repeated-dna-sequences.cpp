9034582f94228c2d44fcde84d39b17fbe3f20dee
Deepak Saarthi P P <165629999+bashleyduttman@users.noreply.github.com>
2025-02-28 21:23:58 +0530

    Time: 38 ms (71.78%), Space: 22.5 MB (75.18%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -2,27 +2,34 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         int l=0,r=0;
-        vector<string>ans;
-        unordered_map<string,int>mpp;
+        unordered_set<string>ans;
+        unordered_set<string>st;
         string sub;
         while(r<s.size()){
             
             
             sub+=s[r];
             if(r-l==9){
-                mpp[sub]++;
+                if(st.find(sub)==st.end()){
+                    st.insert(sub);
+                }
+                else{
+                    ans.insert(sub);
+                }
+               
                 sub.erase(0,1);
                 l++;
 
             }
             r++;
         }
-        for(auto it:mpp){
-            cout<<it.first<<' ';
-            if(it.second>1){
-                ans.push_back(it.first);
-            }
-        }
-        return ans;
+        // for(auto it:mpp){
+        //     cout<<it.first<<' ';
+        //     if(it.second>1){
+        //         ans.push_back(it.first);
+        //     }
+        // }
+        vector<string>a(ans.begin(),ans.end());
+        return a;
     }
 };
