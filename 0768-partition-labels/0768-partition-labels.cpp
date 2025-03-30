7a170939959b7e61d62dc10da8a25f2fb1b844bf
Deepak Saarthi P P <165629999+bashleyduttman@users.noreply.github.com>
2025-03-30 09:22:59 +0530

    Time: 3 ms (30.82%), Space: 8.9 MB (71.56%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char,int>mpp;
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            if(mpp[s[i]]==0){
+                mpp[s[i]]=i+1;
+            }
+            
+        }
+        int l=0,r=0;
+        int maxTime=0;
+        vector<int>ans;
+       while(r<n){
+       
+            if(mpp[s[r]]>maxTime){
+                maxTime=mpp[s[r]]-1;
+            }
+            if(r==maxTime){
+                ans.push_back(r-l+1);
+                l=r+1;
+                maxTime=0;
+            }
+            r++;
+
+        }
+        return ans;
+    }
+};
